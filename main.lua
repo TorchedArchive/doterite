1a4d7aaa7189f7246db26e80ac222b572997a76e
SamuraiStacks <samuraistacksmmlol@gmail.com>
2019-09-26 16:27:04 -0400

    coord fix and fps counter

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
 player = require("player")
 
+function love.load()
+    love.window.setTitle("doterite prototype")
+end
+
 function love.draw()
     player.draw()
+    love.graphics.print("FPS: "..tostring(love.timer.getFPS()))
 end
--- player.lua
+++ player.lua
@@ -7,8 +7,8 @@ player.height = 16 * scale
 
 -- Player coords
 player.coords = {}
-player.coords.x = 10
-player.coords.y = 20
+player.coords.x = 400 - 32
+player.coords.y = 600 - 80
 
 function player.draw()
     love.graphics.rectangle("fill", player.coords.x, player.coords.y, player.width, player.height)
