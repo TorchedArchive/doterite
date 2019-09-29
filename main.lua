5ee1ae31f535d8dc94f8d00fac8d057bf73d07da
SamuraiStacks <samuraistacksmmlol@gmail.com>
2019-09-29 18:26:30 -0400

    added some image assets

--- main.lua
+++ main.lua
@@ -2,8 +2,7 @@
 player = require("player")
 
 -- Load a font
-font = love.graphics.newFont("assets/m5x7.ttf", 55)
-
+font = love.graphics.newFont("assets/m5x7.ttf", 40)
 function love.load()
     -- Set a window title
     love.window.setTitle("doterite prototype")
@@ -13,12 +12,25 @@ function love.load()
 
     -- Actually load the font in the game
     love.graphics.setFont(font)
+
+
+    -- Make assets less blurry and higher quality
+    love.graphics.setDefaultFilter("nearest", "nearest")
+	-- And load them in
+
+	-- These are for the main menu
+    textlogo = love.graphics.newImage("assets/images/textlogo.png")
+    earth = love.graphics.newImage("assets/images/earth.png")	
+    -- Set background color
+    love.graphics.setBackgroundColor(0, 0, 0.1)
 end
 
 function love.draw()
     -- Main Menu
     if state == "menu" then
-        love.graphics.print("Press \"Enter to start\"!", 200, 300)
+        love.graphics.draw(textlogo, 125, 50, 0, 12, 12)
+        love.graphics.draw(earth, -220, 260, 0, 18, 18)
+        love.graphics.print("Press \"Enter to start\"!", 260, 300)
     else
         -- Draws the player's character
         player.draw()
